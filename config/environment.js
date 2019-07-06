06441c469f7771f9f2e030e12e1cfa29d191c3a3
michaeldimmitt <michaelgdimmitt@gmail.com>
2019-07-06 00:36:51 -0400

    npx ember-cli install ember-simple-leaflet-maps
    helpful link: https://stackoverflow.com/a/26425078

--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,7 @@ module.exports = function(environment) {
     environment,
     rootURL: '/',
     locationType: 'auto',
+    LEAFLET_MAPS_API_KEY: 'pk.eyJ1IjoiYm9ic21pdCIsImEiOiJjanhyMTlmcDAwNDVtM21wOHRxaWFsMDFwIn0.PDvotjjbR-qD8ZOH6EDqHA',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -20,6 +21,7 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+
     }
   };
 
